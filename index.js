0221e7008e212c35261e1e2d01c4c0f89fc19134
Dato Lomiashvili <lomiashvili.dato5@gmail.com>
2024-02-24 16:33:34 +0400

    add javascript logic, and change styles

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-function appendOnDisplay() {
-
+const display = document.getElementById("display");
+function appendOnDisplay(character) {
+    display.value += character;
 }
 
 
 function clearDisplay() {
-
+    display.value = '';
 }
 
 function calculate() {
+    try {
 
+        display.value = display.value.length > 0 ? eval(display.value) : '';
+    } catch (error) {
+        display.value = "Error";
+        setTimeout(clearDisplay, 1000);
+    }
 }
